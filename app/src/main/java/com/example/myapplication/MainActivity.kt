55f79448335d341f6a1cdd7704e0c040481af8d6
sense <sense@huayu1.lan>
2023-08-24 16:26:43 +0800

    add app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var diceImage3 : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage3 = findViewById(R.id.dice_image3)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+    private fun show() {
+
+    }
+    private fun rollDice() {
+
+        val randomInt1 = (1..6).random()
+        val drawableResource1 = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage1.setImageResource(drawableResource1)
+
+
+        val randomInt2 = (1..6).random()
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage2.setImageResource(drawableResource2)
+
+
+        val randomInt3 = (1..6).random()
+        val drawableResource3 = when (randomInt3) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage3.setImageResource(drawableResource3)
+
+    }
+}
